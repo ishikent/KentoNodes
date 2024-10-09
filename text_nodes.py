9a0c075db7ffbac6db6e7a8d6ad7c2233e83b3d9
zignize <zignize2023@gmail.com>
2024-10-09 11:15:34 +0900

    Ponyノード

--- text_nodes.py
+++ text_nodes.py
@@ -36,8 +36,50 @@ class Muti2x_TextBox(PromptFormatter):
     return (formatted_prompt,)
 
 
+class Muti2x_Pony_Positive:
+  @classmethod
+  def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt" : ("STRING", {"forceInput":True}),
+            },
+        }
+
+  RETURN_NAMES = ("prompt",)
+  RETURN_TYPES = ("STRING",)
+  FUNCTION = "run"
+  CATEGORY = "00_kento_nodes"
+
+  def run(self, prompt):
+    new_prompt = f"score_9,score_8_up,score_7_up,score_6_up,score_5_up,score_4_up,{prompt},source_anime,rating_explicit"
+    return (new_prompt,)
+
+
+class Muti2x_Pony_Negative:
+  @classmethod
+  def INPUT_TYPES(s):
+        return {}
+
+  RETURN_NAMES = ("prompt",)
+  RETURN_TYPES = ("STRING",)
+  FUNCTION = "run"
+  CATEGORY = "00_kento_nodes"
+
+  def run(self):
+    new_prompt = """
+    source_furry,source_cartoon,
+    monochrome, realistic, rough sketch, fewer digits, extra digits,
+    western_artist,disney,marvel,overwatch,league_of_legends,the_simpsons,realistic,
+    aca,ces,gpo
+    """
+
+    return (new_prompt,)
+
+
 
 NODE_CLASS_MAPPINGS = {
   "PromptFormatter":PromptFormatter,
   "Muti2x_TextBox":Muti2x_TextBox,
+  "Muti2x_Pony_Positive":Muti2x_Pony_Positive,
+  "Muti2x_Pony_Negative":Muti2x_Pony_Negative,
 }
